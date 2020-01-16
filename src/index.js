b983f78db3e2235731f9884345ae5ba3c4123445
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-16 13:24:31 -0600

    5 and 6

--- src/index.js
+++ src/index.js
@@ -3,20 +3,10 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 import api from './api';
-
+import store from'./store';
 import shoppingList from './shopping-list';
 
 const main = function () {
-  api.createItem('pears')
-    .then(res => res.json())
-    .then((_newItem) => {
-      return api.getItems();
-    })
-    .then(res => res.json())
-    .then((items) => {
-      console.log(items);
-    });
-
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
